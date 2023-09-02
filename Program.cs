716a1d2c01a54d6d8be3ad23275a941a691eb168
xuananle <lexuanan18102004@gmail.com>
2023-09-02 22:43:28 +0700

    Adding middleware

--- Program.cs
+++ Program.cs
@@ -2,30 +2,67 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDataProtection();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuthService>();
 builder.Logging.AddConsole();
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+    var authCookie = context.Request.Headers["Cookie"].FirstOrDefault(x => x.Contains("usr"));
+    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+    logger.Log(LogLevel.Information, $"authCookie: {authCookie}");
+    if (authCookie == null)
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Unauthorized");
+        return;
+    }
+
+    await next();
+});
+
+
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
-    
-    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.Contains("usr"));
-    if (authCookie == null)
-    {
-        ctx.Response.StatusCode = 401;
-        return "Unauthorized";
-    }
-
-    return authCookie;
+    return "Hello world"; 
 });
-app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
+app.MapGet("/login", (AuthService auth) =>
 {
-    var protector = idp.CreateProtector("auth-cookie");
-    ctx.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:xuanan")}";
+    auth.SignIn();
     return "ok";
 });
 
-
-
 app.Run();
+
+public class AuthService
+{
+    private readonly IDataProtectionProvider _idp;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor httpContextAccessor)
+    {
+        _idp = idp;
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public AuthService(IDataProtectionProvider idp)
+    {
+        _idp = idp;
+    }
+
+    public AuthService()
+    {
+    }
+
+    public void SignIn()
+    {
+        _idp.CreateProtector("auth-cookie").Protect("usr:xuanan");
+        var protector = _idp.CreateProtector("auth-cookie");
+        _httpContextAccessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:xuanan")}";
+    }
+}
